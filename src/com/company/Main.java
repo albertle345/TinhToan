04cea3383cfb5784c125603e301c77ccf522f734
ThanhHuong98 <ttinhoc15@gmail.com>
2019-10-09 21:01:43 +0700

    [f]

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,16 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("======CHUONG TRINH TINH TOAN DON GIAN========");
         Scanner read = new Scanner(System.in);
-        System.out.println("nhap a:");
-        int a = read.nextInt();
-        System.out.println("nhap b:");
-        int b = read.nextInt();
-
-        read.nextLine();
-
         int chose = 1;
         do {
+            System.out.println("nhap vao so nguyen a:");
+            int a = read.nextInt();
+            System.out.println("nhap so nguyen b:");
+            int b = read.nextInt();
+
+            read.nextLine();
             System.out.println("nhap phep tinh: ");
             String s = read.nextLine();
 
@@ -28,14 +28,14 @@ public class Main {
                 float kq = tinhToan(a,b,s);
                 System.out.println("ket qua: "+kq);
             }
-            System.out.println("Ban co muon tiep tuc: 1: Co || 0: khong");
+            System.out.println("Ban co muon tiep tuc: 1: Co || 0: Thoat");
             chose = read.nextInt();
             if(chose != 1){
+                System.out.println("Thoat chuong trinh tinh toan");
                 break;
             }
 
         }while (chose == 1);
-
 
     }
     private static float tinhToan(int a, int b, String s){
@@ -48,10 +48,10 @@ public class Main {
                 case "-":
                     kq = timHieu(a,b);
                     break;
-                case "x":
+                case "*":
                     kq = timTich(a,b);
                     break;
-                case ":":
+                case "/":
                     kq = timThuong(a,b);
                     break;
                 default:
